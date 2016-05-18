a8715d437fdd468a8bf941bdae24959c703ef90a
Neal Noble <nrnoble@hotmail.com>
2016-05-17 17:32:26 -0700

    Added timer delay parameter on screen for testing.

--- client/main.js
+++ client/main.js
@@ -42,22 +42,26 @@ Template.startGame.helpers({
 Template.pickedBingoBall.helpers({
     pickedBingoNumber() {
         return Template.instance().pickedBingoNumber.get();
-    },
+    }
 });
 
 
 var runStatus = false;
 var buttonInstance = null;
 var PICK_TIMER = 5000;
+
 Template.startGame.events({
     
     'click button'(event, instance) 
     {
         console.log("Entering Template.startGame.events");
+        var gameInstance = null;
+
         // increment the counter when button is clicked
         // instance.counter.set(instance.counter.get() + 1);
         //var pickedNumber = getNextBingoNumberPicked(true);
         //var  id = pickedNumber;
+
         buttonInstance = instance;
         runStatus = !runStatus;
         //$('#startButton').disabled = true;
@@ -71,15 +75,17 @@ Template.startGame.events({
         //$('#bingo'+id).css( "background-color", "yellow" );
         //console.log("bingoNumber: " + '#bingo'+ pickedNumber);
         //instance.pickedBingoNumber.set(pickedNumber);
-
+        var timerDelay = document.getElementById("pickTimerDelay").value;
+        console.log("timerDelay: " + timerDelay);
         // document.getElementById("startButton").disabled = true;
+
         // sleep(3000);
 
         //var myVar = setInterval(myTimer ,1000);
-        var gameInstance = null;
         if (runStatus)
         {
-            gameInstance = setInterval(gameLoop, PICK_TIMER);
+            //gameInstance = setInterval(gameLoop, PICK_TIMER);
+            gameInstance = setInterval(gameLoop, timerDelay);
             document.getElementById("startButton").innerHTML = "Running. Click to Pause";
         }
         else
@@ -95,14 +101,15 @@ Template.startGame.events({
 });
 
 
-
-function myTimer() {
+// Used for testing purposes. Not part of game
+function myTimer()
+{
     var d = new Date();
    // document.getElementById("jumbotron").innerHTML = d.toLocaleTimeString();
     document.getElementById("startButton").innerHTML = d.toLocaleTimeString();
 }
 
-
+// main game loop. This is where bingo numbers are picked during game play.
 function gameLoop(pause, instance )
 {
     console.log("entering function gameLoop");
@@ -113,12 +120,34 @@ function gameLoop(pause, instance )
         $('#bingo' + id).css("background-color", "yellow");
         $('#pickedBingoBall').html(pickedNumber);
 
-
     console.log("exiting function gameLoop");
 }
 
 
+// Returns 12 arrays of representing possible winning rows, columns, and diagonals.
+// 5 columns
+// 5 rows
+// 2 diagonals
+export function verifyBingo()
+{
+    var bingo = [12];
+    bingo[0] = [01, 02, 03, 04, 05];
+    bingo[1] = [06, 07, 8, 9, 10];
+    bingo[2] = [11, 12, 13, 14, 15];
+    bingo[3] = [16, 17, 18, 19, 20];
+    bingo[4] = [21, 22, 23, 24, 25];
 
+    bingo[5] = [01, 06, 11, 16, 21];
+    bingo[6] = [02, 07, 12, 17, 22];
+    bingo[7] = [03, 8, 13, 18, 23];
+    bingo[8] = [04, 9, 14, 19, 24];
+    bingo[9] = [05, 10, 15, 20, 25];
+
+    bingo[10] = [05, 9, 13, 17, 21];
+    bingo[11] = [01, 07, 13, 19, 25];
+
+    return bingo;
+}
 
 
 
