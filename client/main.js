0204833fcc241258177ea55b12c4854dce4a8964
Neal Noble <nrnoble@hotmail.com>
2016-05-23 17:51:53 -0700

    fixed run\pause game. The game now can be paused.

--- client/main.js
+++ client/main.js
@@ -13,7 +13,7 @@ import './main.html';
 
 Template.bingoSquare.onCreated(function bingoSquareOnCreated()
 {
-
+    
 });
 
 // Button used to start a new bingo game
@@ -46,21 +46,24 @@ Template.pickedBingoBall.helpers({
 });
 
 
+
+// runStatus toggles the game between run and pause.
 var runStatus = false;
+
 var buttonInstance = null;
+
+// PICK_TIMER is the delay for between picking bingo ball numbers. 1000 = 1 second
 var PICK_TIMER = 5000;
+
+// gameInstance holds value for the interval timer
+var gameInstance = null;
 
 Template.startGame.events({
     
     'click button'(event, instance) 
     {
         console.log("Entering Template.startGame.events");
-        var gameInstance = null;
 
-        // increment the counter when button is clicked
-        // instance.counter.set(instance.counter.get() + 1);
-        //var pickedNumber = getNextBingoNumberPicked(true);
-        //var  id = pickedNumber;
 
         buttonInstance = instance;
         runStatus = !runStatus;
@@ -79,43 +82,40 @@ Template.startGame.events({
         console.log("timerDelay: " + timerDelay);
         // document.getElementById("startButton").disabled = true;
 
-        // sleep(3000);
 
         //var myVar = setInterval(myTimer ,1000);
         if (runStatus)
         {
-            //gameInstance = setInterval(gameLoop, PICK_TIMER);
+
+            // console.log("BEFORE gameInstance = setInterval(gameLoop, timerDelay): " + gameInstance);
             gameInstance = setInterval(gameLoop, timerDelay);
+            // console.log("AFTER gameInstance = setInterval(gameLoop, timerDelay): " + gameInstance);
+
             document.getElementById("startButton").innerHTML = "Running. Click to Pause";
         }
         else
         {
+            // console.log("BEFORE clearInterval(gameInstance): " + gameInstance);
             clearInterval(gameInstance);
+            // console.log("AFTER clearInterval(gameInstance): " + gameInstance);
             document.getElementById("startButton").innerHTML = "Paused. Click to resume";
         }
 
 
-        console.log("Exiting Template.startGame.events");
+        console.log("Exiting Template.startGame.events. gameInstance=" + gameInstance);
 
     }
 });
 
 
-// Used for testing purposes. Not part of game
-function myTimer()
-{
-    var d = new Date();
-   // document.getElementById("jumbotron").innerHTML = d.toLocaleTimeString();
-    document.getElementById("startButton").innerHTML = d.toLocaleTimeString();
-}
 
-// main game loop. This is where bingo numbers are picked during game play.
+// gameLoop is main game loop. This is where bingo numbers are picked during game play.
+// gameLoop is called by setInterval which then executes every X seconds
 function gameLoop(pause, instance )
 {
     console.log("entering function gameLoop");
     var pickedNumber = getNextBingoNumberPicked(true);
     var id = pickedNumber;
-
 
         $('#bingo' + id).css("background-color", "yellow");
         $('#pickedBingoBall').html(pickedNumber);
@@ -151,3 +151,9 @@ export function verifyBingo()
 
 
 
+// Used for testing purposes. Not part of game
+function myTimer()
+{
+    var d = new Date();
+    document.getElementById("startButton").innerHTML = d.toLocaleTimeString();
+}
